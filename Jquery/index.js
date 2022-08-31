a1f9fa133f2837b3ffc64fecc6b83279744726c7
Vikash Patel <72722514+vikashpatel714@users.noreply.github.com>
2022-08-31 14:15:59 +0530

    Add files via upload

---
+++ Jquery/index.js
@@ -0,0 +1,20 @@
+/* Question - 1
+Create a button , on click of which you should be able to hide a paragraph text, create another button onclick of which this paragraph should be visible
+*/
+
+
+$(".btn-hide").click(function () {
+    $("p").hide();
+  });
+
+  $(".btn-show").click(function () {
+    $("p").show();
+  });
+  
+  
+  /*Question - 2
+  Create an ajax call, by using this api "https://jsonplaceholder.typicode.com/todos/49" and print the title as the heading of the page
+  */
+  $(".btn-ajax").click(function () {
+    $("#heading").load("https://jsonplaceholder.typicode.com/todos/49");
+  });
